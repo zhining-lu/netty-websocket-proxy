918d2e4785ed70b806e29f01318d97c6c447c727
zhining-lu <zhining_lu@163.com>
2020-04-19 21:36:05 +0800

    fix#002 Set SO_RCVBUF and SO_SNDBUF of TCP to improve netty throughput

--- sbin/start-server-wc-proxy.sh
+++ sbin/start-server-wc-proxy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #启动websocket-proxy服务
-nohup java -jar ./netty-websocket-proxy-1.2.0.jar -s -conf="conf/config-example-server.json" 2>&1 &
+nohup java -jar ./netty-websocket-proxy-1.3.0.jar -s -conf="conf/config-example-server.json" 2>&1 &
