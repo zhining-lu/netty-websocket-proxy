a1d8f9b4c598f0bfad593632358a72db2edcf86c
zhining-lu <zhining_lu@163.com>
2020-05-19 23:09:20 +0800

    fix#004 Upgrade to 1.3.1 ，Set memory size

--- sbin/start-local-wc-proxy.sh
+++ sbin/start-local-wc-proxy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-#启动websocket-proxy client服务
+#start websocket-proxy client
 nohup java -Xms512m -Xmx512m -jar ./netty-websocket-proxy-1.3.1.jar -s -conf="conf/config-example-client.json" >/dev/null 2>&1 &
--- sbin/start-server-wc-proxy.sh
+++ sbin/start-server-wc-proxy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-#启动websocket-proxy server服务
+#start websocket-proxy server
 nohup java -Xms512m -Xmx512m -jar ./netty-websocket-proxy-1.3.1.jar -s -conf="conf/config-example-server.json" >/dev/null 2>&1 &
