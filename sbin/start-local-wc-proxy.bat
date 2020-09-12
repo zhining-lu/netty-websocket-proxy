85b3a05ac2df3332f1994de96c3c7ba53e8f44cb
zhining-lu <zhining_lu@163.com>
2020-09-12 22:45:22 +0800

    fix#3007 Add prompt when startup is successful

--- sbin/start-local-wc-proxy.bat
+++ sbin/start-local-wc-proxy.bat
@@ -5,5 +5,5 @@ echo Client service is starting ...
 echo.
 start javaw -Xms512m -Xmx512m -jar ./lib/netty-websocket-proxy-1.3.2.jar -c -conf="./conf/config-example-client.json"
 echo Client service is started.
-echo. & pause
+echo.&echo The proxy program starts successfully, press any key to exit & pause >nul 2>nul
 exit
