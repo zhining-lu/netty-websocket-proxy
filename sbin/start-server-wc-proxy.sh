7f245dcbce0b287998941d8ec354cb18e0ad4c84
zhining-lu <zhining_lu@163.com>
2020-09-12 23:23:44 +0800

    fix#3009 Upgrade to version 1.3.3

--- sbin/start-server-wc-proxy.sh
+++ sbin/start-server-wc-proxy.sh
@@ -6,4 +6,4 @@ basename=`basename $currpath`
 if [ "$basename" = "sbin" ];then
 cd ../
 fi
-nohup java -Xms512m -Xmx512m -jar ./lib/netty-websocket-proxy-1.3.2.jar -s -conf="./conf/config-example-server.json" >/dev/null 2>&1 &
+nohup java -Xms512m -Xmx512m -jar ./lib/netty-websocket-proxy-1.3.3.jar -s -conf="./conf/config-example-server.json" >/dev/null 2>&1 &
