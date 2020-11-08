1191275320a9f151c93191fc135997ee704c32c8
zhining-lu <zhining_lu@163.com>
2020-11-08 18:42:27 +0800

    Upgrade to 1.3.4

--- bin/start-local-wc-proxy.bat
+++ bin/start-local-wc-proxy.bat
@@ -3,7 +3,7 @@ taskkill -f -t -im javaw.exe >nul 2>nul
 cd ../
 echo Client service is starting ...
 echo.
-start javaw -Xms512m -Xmx512m -jar ./lib/netty-websocket-proxy-1.3.3.jar -c -conf="./conf/config-example-client.json"
+start javaw -Xms512m -Xmx512m -jar ./lib/netty-websocket-proxy-1.3.4.jar -c -conf="./conf/config-example-client.json"
 echo Client service is started.
 echo.&echo The proxy program starts successfully, press any key to exit & pause >nul 2>nul
 exit
